436a0f60148668143b3d557577fae3195396339d
Rewriter00x <colossus00x@icloud.com>
2022-06-30 14:23:11 +0300

    Merged changes

--- src/main/java/com/uniprojects/schoolsystem/UI/UserFrame.java
+++ src/main/java/com/uniprojects/schoolsystem/UI/UserFrame.java
@@ -1,6 +1,5 @@
 package com.uniprojects.schoolsystem.UI;
 
-import com.uniprojects.schoolsystem.models.Student;
 import com.uniprojects.schoolsystem.models.User;
 
 import javax.swing.*;
@@ -9,11 +8,23 @@ import java.awt.*;
 public class UserFrame extends JFrame {
     public UserFrame(User user) {
         super();
+
+        this.user = user;
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(ScreenWidth, ScreenHeight);
+        setTitle("School System");
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width/2-getSize().width/2, dim.height/2-getSize().height/2);
+
+        GridBagConstraints constraints = new GridBagConstraints();
     }
 
     protected JPanel mainPanel = new JPanel(new GridBagLayout());
+    protected JLabel mainLabel = new JLabel("School System");
+    protected JLabel nameLabel = new JLabel("Name Surname (maybe photo)");
+
+    private User user;
 
     private static final Font TitleFont = new Font("OCR A Extended", Font.ITALIC, 70);
     private static final Font BaseFont = new Font("OCR A Extended", Font.PLAIN, 30);
